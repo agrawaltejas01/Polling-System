48bd5ea4f0e2ecc8629d06daf2c16208f6d0c578
agrawaltejas01 <agrawaltejas01@gmail.com>
2021-03-08 23:45:44 +0530

    mysql connection setup

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  port: 3000,
+  db: {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'password',
+    database: 'polling_system',
+  },
+};
+
+module.exports = config;
