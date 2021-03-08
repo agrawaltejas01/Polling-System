9e4567d59b828953cd7b294b928d830274ac7754
agrawaltejas01 <agrawaltejas01@gmail.com>
2021-03-08 23:55:15 +0530

    env variables setup

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
   port: 3000,
   db: {
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'password',
-    database: 'polling_system',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '3306',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE || 'polling_system',
   },
 };
 
